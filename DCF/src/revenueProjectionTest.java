62ec731fa629de14bce1f12122b31996e06e830c
Arjun Dubashi <arjun.dubashi@lexmark.com>
2015-07-21 10:10:39 -0400

    Need to add change in working capital

--- DCF/src/revenueProjectionTest.java
+++ DCF/src/revenueProjectionTest.java
@@ -1,7 +1,7 @@
 
 public class revenueProjectionTest {
 	public static void main(String Args[]){
-		revenueProjections company = new revenueProjections(5 ,.1 ,100);
+		revenueProjections company = new revenueProjections(5 ,.2 ,100000,.65,.3 ,.07, 9000);
 		company.calculateRevenueTable();
 		
 		System.out.print(company);
